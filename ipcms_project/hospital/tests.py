e7ec51e26591542a79806c44b122b18d670f51ed
roy07barha <roy07barsha@gmail.com>
2026-08-28 10:49:16 +0530

    Remove credentials from project

--- ipcms_project/hospital/tests.py
+++
@@ -1,9 +0,0 @@
-
-# username - admin
-#password - admin@123
-
-#doc1-doc1@123
-#doc2-doc2@123
-#doc3-doc3@123
-
-#receptionist-user@123
--- ipcms_project/ipcms_project/settings.py
+++ ipcms_project/ipcms_project/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,12 +83,12 @@ WSGI_APPLICATION = 'ipcms_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'ipcms',
-        'USER': 'root',
-        'PASSWORD': 'system',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
