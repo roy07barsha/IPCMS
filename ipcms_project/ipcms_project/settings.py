f02fe22f98aadf1486e8c81948579b86c80be788
roy07barha <roy07barsha@gmail.com>
2026-09-11 01:16:07 +0530

    Prepare project for production deployment

--- ipcms_project/ipcms_project/settings.py
+++ ipcms_project/ipcms_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = [
     '127.0.0.1', 'localhost',
@@ -58,6 +58,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
 
@@ -97,14 +99,15 @@ WSGI_APPLICATION = 'ipcms_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('MYSQLDATABASE', os.getenv('DB_NAME')),
+        'USER': os.getenv('MYSQLUSER', os.getenv('DB_USER')),
+        'PASSWORD': os.getenv('MYSQLPASSWORD', os.getenv('DB_PASSWORD')),
+        'HOST': os.getenv('MYSQLHOST', os.getenv('DB_HOST')),
+        'PORT': os.getenv('MYSQLPORT', os.getenv('DB_PORT')),
     }
 }
 
